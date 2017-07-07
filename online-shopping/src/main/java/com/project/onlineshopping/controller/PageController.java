01032e0359b8c015b92c155ae7eb39a328e3aae9
anurag <singhray9@gmail.com>
2017-07-07 18:20:56 +0530

    UI updated with new Theme.

--- online-shopping/src/main/java/com/project/onlineshopping/controller/PageController.java
+++ online-shopping/src/main/java/com/project/onlineshopping/controller/PageController.java
@@ -9,7 +9,22 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to online-shopping Project");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About us");
+		mv.addObject("userClickabout", true);
+		return mv;
+	}
+	@RequestMapping(value ="/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact us");
+		mv.addObject("userClickcontact", true);
 		return mv;
 	}
 
